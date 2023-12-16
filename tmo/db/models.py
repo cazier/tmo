6c0981dad1f53d358f149d81798b75f0b2c02b9d
Brendan Cazier <520246+cazier@users.noreply.github.com>
2023-12-16 15:34:50 +0000

    Moving around some models

--- tmo/db/models.py
+++ tmo/db/models.py
@@ -15,13 +15,14 @@ class _Charges_Statistics_Link(SQLModel, table=True):
     statistics_id: int = Field(foreign_key="statistics.id", primary_key=True)
 
 
-class _BaseModel(SQLModel):
+class __BaseModel__(SQLModel):
     id: Optional[int] = Field(default=None, primary_key=True)
 
-
-class Subscriber(_BaseModel, table=True):
+class _SubscriberBase(__BaseModel__):
     name: str
     number: str = Field(unique=True, max_length=20)
+
+class Subscriber(_SubscriberBase, table=True):
     number_format: str = Field(default="us", max_length=2)
 
     charges: list["Charges"] = Relationship(back_populates="subscriber")
@@ -29,27 +30,48 @@ class Subscriber(_BaseModel, table=True):
 
     bills: list["Bill"] = Relationship(back_populates="subscribers", link_model=_Subscriber_Bill_Link)
 
+class SubscriberRead(_SubscriberBase):
+    id: int
+    name: str
+    number: str
+    
 
-class Bill(_BaseModel, table=True):
+class _SubscribersList(_SubscriberBase):
+    id: int
+
+class _BillBase(__BaseModel__):
     date: datetime.date = Field(unique=True)
 
+class Bill(_BillBase, table=True):
     charges: list["Charges"] = Relationship(back_populates="bill")
     statistics: list["Statistics"] = Relationship(back_populates="bill")
 
     subscribers: list["Subscriber"] = Relationship(back_populates="bills", link_model=_Subscriber_Bill_Link)
 
+class BillRead(_BillBase):
+    id: int
 
-class Charges(_BaseModel, table=True):
+    charges: list["_ChargesList"]
+    statistics: list["_StatisticsList"]
+    subscribers: list["_SubscribersList"]
+
+
+class BillReadWithSubscribers(_BillBase):
+    id: int
+
+class _ChargesBase(__BaseModel__):
+    line: decimal.Decimal = Field(default=0, max_digits=8, decimal_places=2)
+    phone: decimal.Decimal = Field(default=0, max_digits=8, decimal_places=2)
+    usage: decimal.Decimal = Field(default=0, max_digits=8, decimal_places=2)
+    insurance: decimal.Decimal = Field(default=0, max_digits=8, decimal_places=2)
+
+class Charges(_ChargesBase, table=True):
     bill_id: int = Field(foreign_key="bill.id")
     bill: Bill = Relationship(back_populates="charges")
 
     subscriber_id: int = Field(foreign_key="subscriber.id")
     subscriber: Subscriber = Relationship(back_populates="charges")
 
-    line: decimal.Decimal = Field(default=0, max_digits=8, decimal_places=2)
-    phone: decimal.Decimal = Field(default=0, max_digits=8, decimal_places=2)
-    usage: decimal.Decimal = Field(default=0, max_digits=8, decimal_places=2)
-    insurance: decimal.Decimal = Field(default=0, max_digits=8, decimal_places=2)
 
     statistics: "Statistics" = Relationship(
         back_populates="charges",
@@ -57,20 +79,30 @@ class Charges(_BaseModel, table=True):
         link_model=_Charges_Statistics_Link,
     )
 
+class _ChargesList(_ChargesBase):
+    id: int
 
-class Statistics(_BaseModel, table=True):
+class _StatisticsBase(__BaseModel__):
+    minutes: Optional[int] = Field(default=0)
+    messages: Optional[int] = Field(default=0)
+    data: decimal.Decimal = Field(default=0, max_digits=5, decimal_places=2)
+    
+
+class Statistics(_StatisticsBase, table=True):
     bill_id: int = Field(foreign_key="bill.id")
     bill: Bill = Relationship(back_populates="statistics")
 
     subscriber_id: int = Field(foreign_key="subscriber.id")
     subscriber: Subscriber = Relationship(back_populates="statistics")
 
-    minutes: Optional[int] = Field(default=0)
-    messages: Optional[int] = Field(default=0)
-    data: decimal.Decimal = Field(default=0, max_digits=5, decimal_places=2)
-
-    charges: "Charges" = Relationship(
+    charges: Charges = Relationship(
         back_populates="statistics",
         sa_relationship_kwargs={"uselist": False},
         link_model=_Charges_Statistics_Link,
     )
+
+class _StatisticsList(_StatisticsBase):
+    id: int
+
+class SubscriberReadWithBills(SubscriberRead):
+    bills: list[BillReadWithSubscribers] = []
