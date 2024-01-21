bd62723fd4f8ba51ba4001cec1bc3567347eb71f
Brendan Cazier <520246+cazier@users.noreply.github.com>
2024-01-21 00:46:30 +0000

    Enabling optional SQL syntax highlighting

--- tmo/db/highlight.py
+++ tmo/db/highlight.py
@@ -3,24 +3,42 @@ import sys
 import typing
 
 import rich
-from pygments import highlight
-from pygments.formatters.terminal256 import Terminal256Formatter
-from pygments.lexers.sql import SqlLexer
-from sqlfmt.api import Mode, format_string
+
+try:
+    from pygments import highlight
+    from pygments.formatters.terminal256 import Terminal256Formatter
+    from pygments.lexers.sql import SqlLexer
+    from sqlfmt.api import Mode, format_string
+
+    _COLORIZATION = True
+
+    _mode = Mode(fast=True, no_color=True, no_progressbar=True)
+
+
+except ImportError:
+    logging.getLogger(__name__).warning("Install the `shandy-sqlfmt` package to colorize SQL queries.")
+
+    _COLORIZATION = False
 
 LOGGER_NAME = "sqlalchemy.engine.Engine"
 
-_mode = Mode(fast=True, no_color=True, no_progressbar=True)
+
+def attach_handler() -> None:
+    logging.getLogger(LOGGER_NAME).addHandler(_SqlHandler())
 
 
 def db_print(values: typing.Any, **kwargs: typing.Any) -> None:
     if not isinstance(values, str):
         values = str(values)
 
-    print(highlight(format_string(values, mode=_mode), SqlLexer(), Terminal256Formatter()), **kwargs)
+    if _COLORIZATION:
+        print(highlight(format_string(values, mode=_mode), SqlLexer(), Terminal256Formatter()), **kwargs)
+
+    else:
+        print(values, **kwargs)
 
 
-class SqlHandler(logging.StreamHandler[typing.TextIO]):  # pylint: disable=too-few-public-methods
+class _SqlHandler(logging.StreamHandler[typing.TextIO]):  # pylint: disable=too-few-public-methods
     def __init__(self, stream: None | typing.TextIO = None) -> None:
         if not stream:
             stream = sys.stdout
