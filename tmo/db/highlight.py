f5a6f3d6a6df4fdc20800d8ce7a73ebdf4704f2e
Brendan Cazier <520246+cazier@users.noreply.github.com>
2024-01-22 00:01:53 +0000

    Making SQL highlighting prettier

--- tmo/db/highlight.py
+++ tmo/db/highlight.py
@@ -2,19 +2,37 @@ import logging
 import sys
 import typing
 
-import rich
-from pygments import highlight
-from pygments.formatters.terminal256 import Terminal256Formatter
-from pygments.lexers.sql import SqlLexer
+try:
+    from pygments import highlight
+    from pygments.formatters.terminal256 import Terminal256Formatter
+    from pygments.lexers.sql import SqlLexer
+
+except ImportError:  # pragma: nocover
+    # pylint: disable=invalid-name
+    logger = logging.getLogger("uvicorn")
+    logger.warning("Install the `pygments` package to colorize SQL queries.")
+
+    def highlight(value: typing.Any) -> str:  # type: ignore[no-redef]
+        """Optional def if the pygments package is not available."""
+        return str(value)
+
+    def Terminal256Formatter() -> None:  # type: ignore[no-redef]
+        """Optional def if the pygments package is not available."""
+        return None
+
+    def SqlLexer() -> None:  # type: ignore[no-redef]
+        """Optional def if the pygments package is not available."""
+        return None
+
 
 try:
     from sqlfmt.api import Mode, format_string
 
     _mode = Mode(fast=True, no_color=True, no_progressbar=True)
 
-except ImportError:
+except ImportError:  # pragma: nocover
     logger = logging.getLogger("uvicorn")
-    logger.warning("Install the `shandy-sqlfmt` package to colorize SQL queries.")
+    logger.warning("Install the `shandy-sqlfmt` package to format SQL queries.")
 
     def format_string(source_string: str, mode: Mode) -> str:  # pylint: disable=unused-argument
         """Optional def if the shandy-sqlfmt package is not available."""
@@ -29,7 +47,17 @@ def attach_handler() -> None:
     logging.getLogger(_LOGGER_NAME).addHandler(_SqlHandler())
 
 
-def db_print(values: typing.Any, **kwargs: typing.Any) -> None:
+@typing.overload
+def db_print(values: typing.Any, capture: typing.Literal[True], **kwargs: typing.Any) -> str:
+    ...
+
+
+@typing.overload
+def db_print(values: typing.Any, capture: typing.Literal[False], **kwargs: typing.Any) -> None:
+    ...
+
+
+def db_print(values: typing.Any, capture: bool = False, **kwargs: typing.Any) -> None | str:
     """Wrapper around a regular print call that adds colorization (and optionally formatting) to the SQLAlchemy
     engine.
 
@@ -39,31 +67,38 @@ def db_print(values: typing.Any, **kwargs: typing.Any) -> None:
     if not isinstance(values, str):
         values = str(values)
 
-    print(highlight(format_string(values, mode=_mode), SqlLexer(), Terminal256Formatter()), **kwargs)
+    formatted = highlight(format_string(values, mode=_mode), SqlLexer(), Terminal256Formatter())
+
+    if capture:
+        return formatted
+
+    return print(formatted, **kwargs)
 
 
 class _SqlHandler(logging.StreamHandler[typing.TextIO]):  # pylint: disable=too-few-public-methods
     def __init__(self, stream: None | typing.TextIO = None) -> None:
-        if not stream:
-            stream = sys.stdout
+        stream = stream or sys.stdout
 
         self.__logger = logging.getLogger(_LOGGER_NAME)
-
-        if self.__logger.propagate:
-            self.__logger.propagate = False
-
-        if self.__logger.handlers:
-            self.__logger.handlers.clear()
+        self.__logger.propagate = False
+        self.__logger.handlers.clear()
 
         super().__init__(stream=stream)
 
     def emit(self, record: logging.LogRecord) -> None:
+        """Colorize/format (if able) the logging message and/or arguments.
+
+        Args:
+            record (logging.LogRecord): logging entry
+        """
         if record.msg.startswith("[%s]"):
             if isinstance(record.args, tuple):
                 if not getattr(record.args[-1], "params", False):
                     return
 
-                rich.print(record.args[-1])
-                return
+                record.msg = record.args[-1]
+                record.args = tuple()
 
-        db_print(record.msg, end="")
+        record.msg = db_print(record.msg, capture=True, end="")
+
+        super().emit(record)
