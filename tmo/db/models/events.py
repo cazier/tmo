05f62149c6e3eb5d8e97c67db0c2476cb0648643
Brendan Cazier <520246+cazier@users.noreply.github.com>
2024-10-04 22:14:24 +0000

    fixup! Forgot package

--- tmo/db/models/events.py
+++ tmo/db/models/events.py
@@ -2,32 +2,15 @@ import typing
 
 from sqlalchemy import event
 
-from . import Bill, Detail, Subscriber
+from . import Bill
 
 
-@event.listens_for(Bill.charges, "append")
-@event.listens_for(Bill.details, "append")
-def _update_bill_total(target: Bill, value: "Detail", *_) -> None:
-    target.total = value.total + sum(item.total for item in target.details + target.charges)
+@event.listens_for(Bill.date, "set")
+def _update_bill_date(target: Bill, *_: typing.Any) -> None:
+    if target.date:
+        raise AttributeError("Cannot change the date on an existing row.")
 
 
-@event.listens_for(Subscriber.bills, "append")
-def _update_subscriber_count(target: Subscriber, *_) -> None:
-    target.count = 1 + len(target.bills)
-
-
-@event.listens_for(Detail, "init")
-def _init_detail_total(target: Detail, _: tuple[typing.Any], kwargs: dict[str, typing.Any]):
-    kwargs.setdefault("total", sum(kwargs[name] for name, _ in target.fields_by_annotation(string="$")))
-
-
-# @event.listens_for(Detail.phone, "set")
-# @event.listens_for(Detail.line, "set")
-# @event.listens_for(Detail.insurance, "set")
-# @event.listens_for(Detail.usage, "set")
-# def _update_detail(target: Detail, value: JsonDecimal, oldvalue: JsonDecimal, *_):
-#     if oldvalue != orm.LoaderCallableStatus.NO_VALUE:
-#         value = value - oldvalue
-#         target.bill.total += value
-
-#     target.total = value + sum(getattr(target, name) or 0 for name, _ in target._fields_by_annotation(string="$"))
+# @event.listens_for(Subscriber.bills, "append")
+# def _update_subscriber_count(target: Subscriber, *_) -> None:
+#     target.count = 1 + len(target.bills)
