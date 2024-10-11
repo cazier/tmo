da4fe7ecfa455dd67061ee537f6f536752a1bf65
Brendan Cazier <520246+cazier@users.noreply.github.com>
2024-10-11 20:18:20 +0000

    fixup! fixup! Generic getter

--- tests/lib/test_utilities.py
+++ tests/lib/test_utilities.py
@@ -2,43 +2,40 @@
 
 import secrets
 import typing
+from types import SimpleNamespace as NS
 
 import pytest
 
 from tmo.lib.utilities import get_attr_item
 
 
-class _TestObject:
-    hello: typing.Any
-
-    def __init__(self, hello: typing.Any):
-        self.hello = hello
-
-
 @pytest.mark.parametrize(
-    ("obj", "key"),
+    ("object", "key", "value"),
     (
-        ({"hello": "world"}, ("hello",)),
-        ({"hello": {"goodbye": "world"}}, ("hello", "goodbye")),
-        (_TestObject("world"), ("hello",)),
-        (_TestObject(_TestObject("world")), ("hello", "hello")),
-        ({"hello": _TestObject("world")}, ("hello", "hello")),
-        (_TestObject({"goodbye": "world"}), ("hello", "goodbye")),
-        ({"hello": _TestObject({"goodbye": "world"})}, ("hello", "hello", "goodbye")),
+        ({"a": "world"}, ("a",), "world"),
+        ({"a": {"b": "world"}}, ("a", "b"), "world"),
+        (NS(a="world"), ("a",), "world"),
+        (NS(a=NS(b="world")), ("a", "b"), "world"),
+        ({"a": NS(b="world")}, ("a", "b"), "world"),
+        (NS(a={"b": "world"}), ("a", "b"), "world"),
+        ({"a": NS(a={"b": "world"})}, ("a", "a", "b"), "world"),
+        ({"a": {"b": None}}, ("a", "b"), None),
     ),
-    ids=("map", "map-map", "object", "object-object", "map-object", "object-map", "map-object-map"),
+    ids=("map", "map-map", "object", "object-object", "map-object", "object-map", "map-object-map", "none-value"),
 )
 @pytest.mark.parametrize("success", (True, False, None), ids=("success", "failure", "default"))
-def test_get_attr_item(obj: dict[str, typing.Any] | _TestObject, key: tuple[str, ...], success: bool | None):
+def test_get_attr_item(
+    object: dict[str, typing.Any] | NS, key: tuple[str, ...], value: typing.Any, success: bool | None
+):
     if success is True:
-        assert get_attr_item(obj, *key) == "world"
+        assert get_attr_item(object, *key) == value
 
     if success is False:
         key = (secrets.token_hex(4),)
-        with pytest.raises(KeyError, match=rf"\'{key}\'"):
-            get_attr_item(obj, *key)
+        with pytest.raises(KeyError):
+            get_attr_item(object, *key)
 
     if success is None:
         key = (secrets.token_hex(4),)
         default = secrets.token_hex(4)
-        assert get_attr_item(obj, *key, default=default) == default
+        assert get_attr_item(object, *key, default=default) == default
