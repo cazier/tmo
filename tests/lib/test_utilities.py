dd5a801b705362316fcd01a1b18076be7c575233
Brendan Cazier <520246+cazier@users.noreply.github.com>
2024-10-11 19:29:50 +0000

    fixup! Generic getter

--- tests/lib/test_utilities.py
+++ tests/lib/test_utilities.py
@@ -9,24 +9,36 @@ from tmo.lib.utilities import get_attr_item
 
 
 class _TestObject:
-    hello: str
+    hello: typing.Any
 
-    def __init__(self, hello: str):
+    def __init__(self, hello: typing.Any):
         self.hello = hello
 
 
-@pytest.mark.parametrize("obj", ({"hello": "world"}, _TestObject("world")), ids=("map", "object"))
+@pytest.mark.parametrize(
+    ("obj", "key"),
+    (
+        ({"hello": "world"}, ("hello",)),
+        ({"hello": {"goodbye": "world"}}, ("hello", "goodbye")),
+        (_TestObject("world"), ("hello",)),
+        (_TestObject(_TestObject("world")), ("hello", "hello")),
+        ({"hello": _TestObject("world")}, ("hello", "hello")),
+        (_TestObject({"goodbye": "world"}), ("hello", "goodbye")),
+        ({"hello": _TestObject({"goodbye": "world"})}, ("hello", "hello", "goodbye")),
+    ),
+    ids=("map", "map-map", "object", "object-object", "map-object", "object-map", "map-object-map"),
+)
 @pytest.mark.parametrize("success", (True, False, None), ids=("success", "failure", "default"))
-def test_get_attr_item(obj: dict[str, typing.Any] | _TestObject, success: bool | None):
+def test_get_attr_item(obj: dict[str, typing.Any] | _TestObject, key: tuple[str, ...], success: bool | None):
     if success is True:
-        assert get_attr_item(obj, "hello") == "world"
+        assert get_attr_item(obj, *key) == "world"
 
     if success is False:
-        key = secrets.token_hex(4)
+        key = (secrets.token_hex(4),)
         with pytest.raises(KeyError, match=rf"\'{key}\'"):
-            get_attr_item(obj, key)
+            get_attr_item(obj, *key)
 
     if success is None:
-        key = secrets.token_hex(4)
+        key = (secrets.token_hex(4),)
         default = secrets.token_hex(4)
-        assert get_attr_item(obj, key, default=default) == default
+        assert get_attr_item(obj, *key, default=default) == default
--- tmo/lib/utilities.py
+++ tmo/lib/utilities.py
@@ -3,13 +3,26 @@ import typing
 _unset = object()
 
 
-def get_attr_item(obj: typing.Any, key: typing.Any, *, default: typing.Any = _unset) -> typing.Any:
-    if hasattr(obj, key):
-        return getattr(obj, key)
-    try:
-        return obj[key]
-    except (KeyError, TypeError):
-        if default is not _unset:
-            return default
+def get_attr_item(obj: typing.Any, *key: typing.Any, default: typing.Any = _unset) -> typing.Any:
+    if not key:
+        return obj
 
-    raise KeyError(f"'{key}'")
+    _key, *_others = key
+
+    if hasattr(obj, _key):
+        result = getattr(obj, _key)
+
+    else:
+        try:
+            result = obj[_key]
+
+        except (KeyError, TypeError):
+            if default is not _unset:
+                return default
+
+            raise KeyError(f"'{_key}'")
+
+    if result is _unset:
+        return default
+
+    return get_attr_item(result, *_others, default=default)
