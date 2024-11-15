b40e21065d1d901e1a8622c2f2639a93f4b7bb8d
Brendan Cazier <520246+cazier@users.noreply.github.com>
2024-11-15 23:15:04 +0000

    Using test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,12 @@
 # mypy: disable-error-code="no-untyped-def"
-
-import calendar
-import datetime
-import random
-import typing
+import collections
+import json
+import pathlib
+import re
 
 import fastapi.testclient
 import pytest
-from sqlmodel import Session, SQLModel
+from sqlmodel import Session, SQLModel, text
 
 from tmo import config
 
@@ -17,54 +16,34 @@ SERVICES: tuple[str, ...] = ("netflix", "hulu")
 FIELDS: tuple[str, ...] = ("phone", "line", "insurance", "usage", "minutes", "messages", "data")
 
 
-# @pytest.fixture(autouse=True, scope="session")
-# def _setup_database(subscribers: list[dict[str, str]], bills: set[datetime.date], database: dict[str, typing.Any]):
-#     def _numbers() -> dict[str, int | float]:
-#         def _number(kind: str) -> int | float:
-#             match kind:
-#                 case "phone" | "line" | "insurance" | "usage" | "data":
-#                     return float(f"{random.randint(0, 30)}.{random.randint(0, 99)}")
-
-#                 case _:  # "minutes" | "messages":
-#                     return random.randint(0, 10000)
-
-#         return {key: _number(key) for key in FIELDS}
-
-#     for month in bills:
-#         database[month.strftime("%Y.%m.%d")] = {
-#             "subscribers": [{**subscriber, **_numbers()} for subscriber in subscribers],
-#             "other": {random.choices(SERVICES, [90, 10], k=1)[0]: random.randint(5, 10)},
-#         }
-
-#     yield
-
-
-@pytest.fixture(scope="session")
-def bills():
-    def _get_date(year: int, month: int) -> datetime.date:
-        return datetime.date(year, month, random.randint(1, calendar.monthrange(year, month)[1]))
-
-    start = random.randint(2000, 2010)
-    return {_get_date(start + year, month) for month in range(1, 13) for year in range(YEARS)}
-
-
-@pytest.fixture(scope="session")
-def database():
-    _db: dict[str, typing.Any] = {}
-    yield _db
-
-
 @pytest.fixture(scope="session")
 def session():
-    with config.patch(database={"dialect": "memory"}):
+    with config.patch(database={"dialect": "memory", "echo": True}):
         from tmo.db.engines import start_engine
 
-        engine = start_engine()
+        engine = start_engine(connect_args={"check_same_thread": False})
 
-        with Session(engine) as _session:
+        with Session(engine, autoflush=False) as _session:
             yield _session
 
         SQLModel.metadata.drop_all(engine)
+
+
+@pytest.fixture(scope="module")
+def database(session: Session):
+    raw_data = pathlib.Path(__file__).parent.joinpath("data.sql").read_text("ascii")
+
+    data = collections.defaultdict(list)
+    pattern = re.compile(r"INSERT INTO (\w+) VALUES\((.*)\);")
+
+    for line in raw_data.splitlines():
+        session.exec(text(line))
+
+        if match := pattern.match(line):
+            table, _fields = match.groups()
+            data[table].append([json.loads(field.strip()) for field in _fields.strip().split(",")])
+
+    yield data
 
 
 @pytest.fixture(scope="session")
--- tests/routers/test_subscriber.py
+++ tests/routers/test_subscriber.py
@@ -1,22 +1,27 @@
-import faker
+# mypy: disable-error-code="has-type,no-untyped-def"
+import random
+
 import pytest
 from fastapi.testclient import TestClient
-from sqlmodel import Session
 
-from tests.helpers import phone_number
 from tmo.db.models.tables import Subscriber
 
-
-@pytest.fixture(scope="module")
-def subscriber(session: Session, client: TestClient):
-    subscriber = Subscriber(number=phone_number(), name=faker.Faker().name())
-    session.add(subscriber)
-    session.commit()
-
-    yield subscriber
+pytestmark = [pytest.mark.usefixtures("database")]
 
 
-def test_get_subscribers(client: TestClient, subscriber: Subscriber):
+@pytest.fixture
+def subscriber(database: dict[str, list[tuple[str, ...]]]):
+    id, number, name, format, count = random.choice(database["subscriber"])
+    return Subscriber.model_validate(dict(id=id, number=number, name=name, number_format=format, count=count))
+
+
+def test_get_subscribers(client: TestClient):
     response = client.get("/api/subscriber")
     assert response.status_code == 200
-    assert len(response.json()) == 1 and response.json() == "1234"
+    assert len(response.json()) == 11
+
+
+def test_get_subscriber(client: TestClient, subscriber: Subscriber):
+    breakpoint()
+    response = client.get(f"/api/subscriber/{subscriber.id}")
+    assert response.status_code == 200 == subscriber
